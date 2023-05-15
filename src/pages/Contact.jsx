024bf59f85b3b7f14422462c861bc88eec52b1d1
skouch <skouchy24@gmail.com>
2023-05-15 16:43:48 -0600

    pages & layout structured initially

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+      <div id="contact">
+        <p>CONTACT PAGEEE</p>
+      </div>
+    );
+  }
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,7 @@
+export default function Resume() {
+    return (
+      <div id="resume">
+        <p>RESUMEEE</p>
+      </div>
+    );
+  }
